653bd4adf2642ee2fa3112d6cae219050afdd546
AndreaDistefanoDev7 <andre.distefano99@gmail.com>
2026-03-22 17:07:15 +0100

    first task

---
+++ assets/main.js
@@ -0,0 +1,20 @@
+console.log('juveeee');
+
+const racingBike =[
+    {
+        id:1,
+        name:'Bianchi',
+        weight: 10
+
+    },
+    {
+        id:2,
+        name: 'Scott',
+        weight:7
+    }
+]
+ const moreWeight= racingBike[1]
+ console.log(moreWeight);
+ console.log(racingBike[1]['weight']);
+ 
+ 
